d6cb4fd1fcb0e7ab8e829d67035b22aaec829bee
Nizar Rahme <nizar.w.rahmah@gmail.com>
2018-02-07 15:57:45 +0200

    initial commit

---
+++ src/components/Login/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Login = () => {
+  return <div>login with Github</div>
+}
+
+export default Login;
---
+++ src/components/MainLayout/index.js
@@ -0,0 +1,24 @@
+import React, { Fragment } from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+import Header from '../Header';
+import Login from '../Login';
+import LoginAdmin from '../LoginAdmin';
+import Footer from '../Footer';
+
+const MainLayout = ({ match }) => {
+	return (
+		<Fragment>
+			<Header />
+			<Switch>
+				<Route path={`${match.url}/home`} render={() => <h1>home</h1>} />
+				<Route path={`${match.url}/login-admin`} component={LoginAdmin} />
+				<Route path={`${match.url}/login`} component={Login} />
+				<Redirect from={match.url} to={`${match.url}/home`} />
+			</Switch>
+			<Footer />
+		</Fragment>
+	);
+};
+
+export default MainLayout;
