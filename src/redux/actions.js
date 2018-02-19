1cb0974e69e89ac80ff87b534b7fce30c77bfab9
niloofarmotamed <niloofar.motamed@gmail.com>
2018-02-19 15:18:03 +0200

    Remove SORT_CLASSES action

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,7 +1,6 @@
 
 export const LOG_IN = 'LOG_IN';
 export const LOG_OUT = 'LOG_OUT';
-export const SORT_CLASSES = 'SORT_CLASSES'
 export const GET_CLASSES = 'GET_CLASSES'
 export const logIn = user => ({ type: LOG_IN, payload: user });
 export const logOut = user => ({ type: LOG_OUT });
