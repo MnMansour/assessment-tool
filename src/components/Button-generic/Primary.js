095cdcffc7adf4addba866eaede78b32cf2aa4b5
Olaoluwa Alabi <ayoalabi0@gmail.com>
2018-02-20 11:51:31 +0200

    Generic-button-component-3

---
+++ src/components/Button-generic/Primary.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+
+export class primaryButton extends Component{
+    render(){
+        return(
+            <div className="primary">
+                <button>Button</button>
+            </div>
+        )
+    }
+}
