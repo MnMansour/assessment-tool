a614406d27356845e3339640f11567aeb158dc3a
Anh Hoang Vo <vohoanganhkhtn@gmail.com>
2018-02-21 15:11:54 +0200

    Fixed size screen for responsive

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,15 +1,15 @@
-import { combineReducers } from 'redux';
-import { LOG_IN, LOG_OUT } from './actions';
+import { combineReducers } from "redux";
+import { LOG_IN, LOG_OUT } from "./actions";
 
 const user = (state = {}, { type, payload }) => {
-	switch (type) {
-		case LOG_IN:
-			return { ...payload };
-		case LOG_OUT:
-			return {};
-		default:
-			return state;
-	}
+    switch (type) {
+        case LOG_IN:
+            return { ...payload };
+        case LOG_OUT:
+            return {};
+        default:
+            return state;
+    }
 };
 
 export default combineReducers({ user });
