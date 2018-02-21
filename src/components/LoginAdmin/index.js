4a9adcfdd4df695746cb145de1b777025c3e9dcc
Anh Hoang Vo <vohoanganhkhtn@gmail.com>
2018-02-21 13:40:11 +0200

    fixed some Eslint errors

--- src/components/LoginAdmin/index.js
+++ src/components/LoginAdmin/index.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
-  return <div>login as an admin</div>
-}
+    return <div>login as an admin</div>;
+};
 
 export default Login;
