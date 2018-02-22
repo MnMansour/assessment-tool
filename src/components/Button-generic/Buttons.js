e892571aeedf61ac57fab108fe22caea9cf31b4e
Olaoluwa Alabi <ayoalabi0@gmail.com>
2018-02-22 13:43:10 +0200

    image props to generic-button

--- src/components/Button-generic/Buttons.js
+++ src/components/Button-generic/Buttons.js
@@ -1,14 +1,14 @@
 import React, {Component} from "react";
 import "./button.css";
 import PropTypes from "prop-types";
-import icon from "../../assets/loading.svg";
+//import icon from "../../assets/loading.svg";
 
 export class PrimaryButton extends Component{
 	render(){
 		return(
 			<div className="primary">
 				<button className={this.props.class || "primary-button"} type={this.props.type || "button"} >
-					<img src={this.props.icon || icon} alt="icon" className="primary-button-img"/>
+					<img src={this.props.icon} alt="icon" className={this.props.image||"primary-button-img" }/>
 					{this.props.label || "Generic -Button"}</button>
 			</div>
 		);
@@ -20,7 +20,7 @@ export class SecondaryButton extends Component{
 		return(
 			<div className="secondary">
 				<button className={this.props.class || "secondary-button"} type={this.props.type || "button"}>  
-					<img src={this.props.icon || icon} alt="icon" className="secondary-button-img"/>
+					<img src={this.props.icon || "no-icon"} alt="icon" className={this.props.image||"secondary-button-img" }/>
 					{this.props.label || "Generic -Button"}</button>
 			</div>
 		);
@@ -32,7 +32,7 @@ export class TertiaryButton extends Component{
 		return(
 			<div className="tertiary">
 				<button className={this.props.class || "tertiary-button"} type={this.props.type || "button"}>
-					<img src={this.props.icon || icon} alt="icon" className="tertiary-button-img"/>
+					<img src={this.props.icon || "no-icon"} alt="icon" className={this.props.image||"tertiary-button-img" }/>
 					{this.props.label || "Generic -Button"}</button>
 			</div>
 		);
@@ -43,19 +43,22 @@ PrimaryButton.propTypes={
 	icon:PropTypes.node,
 	label:PropTypes.string,
 	class:PropTypes.string,
-	type:PropTypes.string
+	type:PropTypes.string,
+	image:PropTypes.string
 };
 
 SecondaryButton.propTypes={
 	icon:PropTypes.node,
 	label:PropTypes.string,
 	class:PropTypes.string,
-	type:PropTypes.string
+	type:PropTypes.string,
+	image:PropTypes.string
 };
 
 TertiaryButton.propTypes={
 	icon:PropTypes.node,
 	label:PropTypes.string,
 	class:PropTypes.string,
-	type:PropTypes.string
+	type:PropTypes.string,
+	image:PropTypes.string
 };
