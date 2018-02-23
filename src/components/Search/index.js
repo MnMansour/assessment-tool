25ead445d7409c7fba88955a1b68d89fc9996cc9
Sanjog <sanjogstha7@gmail.com>
2018-02-23 14:27:08 +0200

    commenting fixed

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,46 +1,49 @@
 import React from 'react';
-import './search.css';
 import propTypes from 'prop-types';
+import './search.css';
 
 class Search extends React.Component {
-  constructor(props){
-    super(props);
+	constructor(props){
+		super(props);
 
-    this.state = {
-      name: ''
-    }
-  }
-  handleChange = (e)=>{  
-    const name = e.target.value;
-    this.setState({
-      name 
-    })
-    const filterdList = this.props.classes.filter(singleClass =>{
-      return singleClass.name.toLowerCase().includes(name.toLowerCase())
-    })
-    this.props.onChange(filterdList)
-  }
-  render() {     
-    return (
-      <section className="container">
-        <h2>Teacher Dashboard</h2>
-        <label htmlFor="classes" className="label-classes">Classes</label><br/>
-          <i className="fa fa-search"></i>
-          <input
-             type="text"
-             id="classes"
-             className="search-input"
-             placeholder="search by name"
-             value={this.state.name}
-             onChange={e=>this.handleChange(e)}
-           />
-      </section>
-    );
-  }
+		this.state = {
+			name: ''
+		};
+	}
+handleChange = (e)=>{  
+	const name = e.target.value;
+	
+	this.setState({
+		name 
+	});
+	const filterdList = this.props.classes.filter(singleClass =>{
+		return singleClass[this.props.searchKey].toLowerCase().includes(name.toLowerCase());
+	});
+	this.props.onChange(filterdList);
+    
 }
+render() {
+	return (
+		<section className="container">
+			<h2>Teacher Dashboard</h2>
+			<label htmlFor="classes" className="label-classes">{this.props.class}</label><br/>
+			<i className="fa fa-search"></i>
+			<input
+				type="text"
+				id="classes"
+				className="search-input"
+				placeholder={`search by ${this.props.searchKey}`}
+				value={this.state.name}
+				onChange={e=>this.handleChange(e)}
+			/>
+		</section>
+	);
+}
+}
+
+Search.propTypes = {
+	classes: propTypes.array
+};
 
 export default Search;
 
-Search.propTypes = {
-  classes: propTypes.array
-}
