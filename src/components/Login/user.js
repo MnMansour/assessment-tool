f7a40e03d45750181fe03fe2dab9a264fd44899d
Sannu8 <smahat.official@gmail.com>
2018-03-09 10:46:00 +0200

    Fix merge conflicts

--- src/components/Login/user.js
+++ src/components/Login/user.js
@@ -1,15 +1,9 @@
 import React, { Component } from "react";
 import "./login.css";
-<<<<<<< HEAD
-import { connect } from "react-redux";
-import { logOut } from "../../redux/actions";
-import { UserSelector } from "../../redux/selectors";
-=======
 import { withRouter } from "react-router-dom";
-import {connect} from "react-redux";
-import {resetState} from "../../redux/actions";
-import {userSelector} from "../../redux/selectors";
->>>>>>> feature/login
+import { connect } from "react-redux";
+import { resetState } from "../../redux/actions";
+import { userSelector } from "../../redux/selectors";
 import PropTypes from "prop-types";
 
 class User extends Component {
@@ -19,8 +13,12 @@ class User extends Component {
 		window.location.href = "/app/home";
 	};
 
-<<<<<<< HEAD
-	render() {
+	handleLogout = () => {
+		this.props.logout();
+		this.props.history.push("/app/home");
+	};
+
+	CorrectPassword = () => {
 		return (
 			<div>
 				{`You are welcome ${this.props.user[0].firstName}...`}
@@ -33,65 +31,32 @@ class User extends Component {
 				</form>
 			</div>
 		);
+	};
+
+	render() {
+		return (
+			<div>
+				{this.props.user[0].flag === "error"
+					? this.handleLogout()
+					: this.CorrectPassword()}
+			</div>
+		);
 	}
 }
 const mapStateToProps = state => {
 	return {
-		user: UserSelector(state)
+		user: userSelector(state)
 	};
 };
 const mapDispatchToProps = dispatch => {
 	return {
-		logout: () => dispatch(logOut())
-=======
-handleLogout=()=>{
-	this.props.logout();
-	this.props.history.push("/app/home");
-}
-
-CorrectPassword=()=>{
-	return(
-		<div>
-			{`You are welcome ${this.props.user[0].firstName}...`}
-			<form onSubmit={(e)=>{this.handleLogout(e);}}>
-				<input type="submit" value="Logout"/>
-			</form>
-		</div>
-	);
-}
-
-
-render() {
-	return (
-		<div>
-			{this.props.user[0].flag==="error"? this.handleLogout():this.CorrectPassword()}
-		</div>
-	);
-}
-}
-const mapStateToProps = (state)=>{
-	return {
-		user: userSelector(state)
-	};
-};
-const mapDispatchToProps = (dispatch)=>{
-	return { 
-		logout: ()=>dispatch(resetState()),
->>>>>>> feature/login
+		logout: () => dispatch(resetState())
 	};
 };
 
 User.propTypes = {
-<<<<<<< HEAD
-	user: PropTypes.object,
+	user: PropTypes.any,
 	logout: PropTypes.func
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(User);
-=======
-	user: PropTypes.any,
-	logout:PropTypes.func
-};
-
 export default connect(mapStateToProps, mapDispatchToProps)(withRouter(User));
->>>>>>> feature/login
