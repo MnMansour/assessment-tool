49b8f33787f6d1f805d64e4718672e7bc358e117
Mohammed Mansoor <mohamednmansour.90@gmail.com>
2018-07-24 12:49:43 +0300

    eject react app and change style to scss
    Signed-off-by: Mohammed Mansoor <mohamednmansour.90@gmail.com>

--- src/components/MainLayout/index.js
+++ src/components/MainLayout/index.js
@@ -5,14 +5,13 @@ import Header from '../Header';
 import Login from '../Login';
 import LoginAdmin from '../LoginAdmin';
 import Footer from '../Footer';
-import signupForm from '../Signup';
 
 const MainLayout = ({ match }) => {
 	return (
 		<Fragment>
 			<Header />
 			<Switch>
-				<Route path={`${match.url}/home`} component={signupForm} />
+				<Route path={`${match.url}/home`} component={Login} />
 				<Route path={`${match.url}/login-admin`} component={LoginAdmin} />
 				<Route path={`${match.url}/login`} component={Login} />
 				<Redirect from={match.url} to={`${match.url}/home`} />
