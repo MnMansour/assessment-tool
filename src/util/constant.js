d153d892ef59315a5c0fa4c8a2e913c50600944a
Mohammed Mansoor <mohamednmansour.90@gmail.com>
2018-07-26 10:47:42 +0300

    create constant file
    Signed-off-by: Mohammed Mansoor <mohamednmansour.90@gmail.com>

---
+++ src/util/constant.js
@@ -0,0 +1,5 @@
+export const EDUCATION = 'Education';
+export const EXPERIENCE = 'Experience';
+export const SKILLS = 'Skills';
+export const ASSIGNMENTS = 'Assignments';
+export const PROJECTS = 'Projects';
