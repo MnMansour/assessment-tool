acc3b9f7590195bcac05a4b87696147eabadb682
Mohammed Mansoor <mohamednmansour.90@gmail.com>
2018-08-12 01:31:23 +0300

    make the router only on Root
    Signed-off-by: Mohammed Mansoor <mohamednmansour.90@gmail.com>

--- src/components/Root/index.js
+++ src/components/Root/index.js
@@ -1,22 +1,34 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { applyMiddleware ,createStore } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-
 import reducers from '../../redux/reducers';
-import App from '../App';
+
+import Header from '../Header';
+import Login from '../Login';
+import Footer from '../Footer';
+import Profile from '../Profile';
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
 
-const store = createStoreWithMiddleware(
-	reducers,
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = createStoreWithMiddleware(reducers);
 
 const Root = () => (
 	<Provider store={store}>
-		<App />
+		<BrowserRouter>
+			<div className="Root">
+				<Header/>
+				<div className="body">
+					<Switch>
+						<Route path='/Login' component={Login} />
+						<Route exact path='/' component={Profile} />
+					</Switch>
+				</div>
+				<Footer />
+			</div>
+		</BrowserRouter>
 	</Provider>
 );
 
