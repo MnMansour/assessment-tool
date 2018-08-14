75bb9b11c587f2799834de17b957dc42530565af
Mohammed Mansoor <mohamednmansour.90@gmail.com>
2018-08-14 18:01:20 +0300

    clean firebase file
    Signed-off-by: Mohammed Mansoor <mohamednmansour.90@gmail.com>

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -1,6 +1,7 @@
 import  firebase from 'firebase/app';
 import 'firebase/database';
 import 'firebase/auth';
+import 'firebase/storage';
 
 import axios from 'axios';
 
@@ -16,113 +17,15 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 export const database = firebase.database();
 export const auth = firebase.auth();
-
-
-
-  export const signUp = async (email, password) => {
-    try{
-
-      //check if email exists before passing to firebase, send the rest data to database
-      let userSign = await firebase.auth().createUserWithEmailAndPassword(email, password);
-      return userSign;
-
-    }catch(error){
-        console.error(error);
-        return error;
-    }
-  };
-
-  export const doSignInWithEmailAndPassword =  (email, password) => {
-    return firebase.auth().signInWithEmailAndPassword(email.trim() , password)
-};
-
-export const writeData = (title, data) => {
-  firebase.database().ref(title).set({
-    data
-  });
-};
-
-export const postData = (dataToPost, title) => {
-    const url = `https://assesment-tool.firebaseio.com/${title}.json`;
-    const data = {
-        method: 'POST',
-        body: JSON.stringify(dataToPost),
-        };
-    try{
-        axios.post(url , data)
-        .then(response => response.status)
-    }catch(error){
-        console.error(error);
-        return error;
-    }
-};
-
-
-export const getData = async (title) => {
-    const url = `https://assesment-tool.firebaseio.com/${title}.json`;
-    //const myInit = { method: 'GET', type : 'application/json'};
-    try{
-        let fetchedData = await axios.get(url).then(response =>  response.data );
-        return fetchedData;
-    }catch(error){
-        console.error(error);
-        return error;
-    }
-}
-
-export const loginWithGithub = async () => {
-    const provider = new firebase.auth.GithubAuthProvider();
-    const providerWithRepo = provider.addScope('repo');
-    // get all allowed emails
-    const allowedEmails =  await getData('allowedEmails');
-    const authDetails = await firebase.auth().signInWithPopup(providerWithRepo).then(function(result) {
-      // check user email allowed to signIn
-      const userAllowed = Object.values(allowedEmails).includes(result.user.email);
-      if (userAllowed) {
-        const user = result.user;
-        const profile = result.additionalUserInfo.profile;
-        return {user: user, repo: profile};
-      }
-      else return false
-    }).catch(function(error) {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      const credential = error.credential;
-      return {errorCode, errorMessage, email, credential}
-    });
-  return authDetails;
-}
-
-
-export const signOut = async () => {
-    try{
-        const signOutData = await firebase.auth().signOut().then(function() {});
-        return signOutData;
-    }catch(error){
-        console.error(error);
-        return error;
-    }
-}
-
-export const doSendPasswordResetEmail = (email) => {
-    const auth = firebase.auth();
-    return auth.sendPasswordResetEmail(email)
-}
+export const githubProvider = new firebase.auth.GithubAuthProvider();
+export const storage = firebase.storage().ref();
 
 export const linkloginAccounts = async (email, password) => {
-    try{
-        let credential = await firebase.auth.EmailAuthProvider.credential(email, password);
-        const user = await firebase.auth().currentUser.linkAndRetrieveDataWithCredential(credential).then((usercred)=> {
-                        let user = usercred.user;
-                        console.log("Account linking success", user);
-                    })
-        return user;
-    }catch(error){
-        console.error(error);
-        return error;
-    }
+  try {
+    let credential = await firebase.auth.EmailAuthProvider.credential(email, password);
+    return await firebase.auth().currentUser.linkAndRetrieveDataWithCredential(credential);
+  }catch(error){
+    console.error(error);
+    return error;
+  }
 }
