8b5b588869b6077d52be76c46f46d22ef091f8c5
Mohammed Mansoor <mohamednmansour.90@gmail.com>
2018-08-14 17:26:40 +0300

    implement actions
    Signed-off-by: Mohammed Mansoor <mohamednmansour.90@gmail.com>

--- src/redux/actions/actions.js
+++ src/redux/actions/actions.js
@@ -1,9 +1,14 @@
-import {auth} from '../../util/firebase';
+import {auth, database, githubProvider} from '../../util/firebase';
 
 export const LOG_IN = 'LOG_IN';
 export const LOG_OUT = 'LOG_OUT';
-export const USER_STATUS = 'USER_STATUS'
-export const GET_USER = 'GET_USER'
+export const USER_STATUS = 'USER_STATUS';
+export const GET_USER = 'GET_USER';
+export const USER_DB_STATUS = 'USER_DB_STATUS';
+export const GET_DB_USERS = 'GET_DB_USERS'
+export const ALLOWED_STATUS = 'ALLOWED_STATUS'
+export const ALLOWED_USERS = 'ALLOWED_USERS'
+
 
 export const logIn = user => ({ type: LOG_IN, payload: user });
 export const logOut = user => ({ type: LOG_OUT });
@@ -25,4 +30,60 @@ export function getUser() {
       });
     });
   };
+}
+
+export function getDbUsers() {
+  return dispatch => {
+    dispatch({
+      type: USER_DB_STATUS,
+      payload: true
+    });
+    database.ref('users').on('value', db => {
+      dispatch({
+        type: GET_DB_USERS,
+        payload: db.val()
+      });
+      dispatch({
+        type: USER_DB_STATUS,
+        payload: false
+      });
+    });
+  };
+}
+
+export function getDbAllowedUsers() {
+  return dispatch => {
+    dispatch({
+      type: ALLOWED_STATUS,
+      payload: true
+    });
+    database.ref('allowedEmails').on('value', db => {
+      dispatch({
+        type: ALLOWED_USERS,
+        payload: db.val()
+      });
+      dispatch({
+        type: ALLOWED_STATUS,
+        payload: false
+      });
+    });
+  };
+}
+
+export function loginWithGithub() {
+  return dispatch => {
+    auth.signInWithPopup(githubProvider)
+  }
+}
+
+export function login(email, password) {
+  return dispatch => auth.signInWithEmailAndPassword(email, password);
+}
+
+export function logout() {
+  return dispatch => auth.signOut();
+}
+
+export function writeToDatabase(collection, data){
+  return dispatch => database.ref(collection).child(data.uid).set({data})
 }
