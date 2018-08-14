76a39d657c9f7cd075d6ba5e7b36a34cbb5b7455
Mohammed Mansoor <mohamednmansour.90@gmail.com>
2018-08-14 17:27:39 +0300

    update index reducer
    Signed-off-by: Mohammed Mansoor <mohamednmansour.90@gmail.com>

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,14 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import UserReducer from './UserReducer';
+import LoadingReducer from './LoadingReducer';
+import DbUserReducer from './DbUserReducer';
+import DbAllowedUsersReducer from './DbAllowedUsersReducer';
 
 export default combineReducers({
 	 form: formReducer,
 	 user: UserReducer,
+	 loading: LoadingReducer,
+	 dbUsers: DbUserReducer,
+	 dbAllowedUsers: DbAllowedUsersReducer
   });
