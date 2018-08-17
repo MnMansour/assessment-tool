067939f0275ff4613319271bb7eab6946191de58
Mohammed Mansoor <mohamednmansour.90@gmail.com>
2018-08-17 22:29:09 +0300

    create education and experience component for section body
    Signed-off-by: Mohammed Mansoor <mohamednmansour.90@gmail.com>

---
+++ src/components/Profile/EducationAndExperience.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import FormModal from '../Modal';
+import editButton from '../../assets/edit-button.png';
+class EducationAndExperience extends Component {
+
+  state = {
+    modalIsOpen: false
+  }
+
+  openModal = () => {
+    this.setState({modalIsOpen: true});
+  }
+
+  closeModal = () => {
+    this.setState({modalIsOpen: false});
+  }
+
+  modal = (title) => {
+    const {modalIsOpen} = this.state,
+          {Data} = this.props;
+    return <FormModal title={title} modalIsOpen={modalIsOpen} closeModal={this.closeModal} Data={Data}/>
+  }
+
+  render() {
+    const {Data: {place, title, dgree, id, location, toDate, fromDate, description}, enableEdit} = this.props;
+    const options = { year: 'numeric', month: 'long' };
+    const startDate = new Date(fromDate).toLocaleDateString("en-US", options)
+    const endDate = toDate? new Date(toDate).toLocaleDateString("en-US", options) : 'Present';
+    return(
+      <div className="section__body__education-work">
+        <img className="edit" src={editButton} alt="edit" onClick={this.openModal}/>
+        <div className="place">{place}</div>
+        <div className="title">{dgree ? dgree + ', ': ''}{title}</div>
+        <div className="date">{startDate} - {endDate}</div>
+        <div className="location">{location}</div>
+        <div className="description" dangerouslySetInnerHTML={{__html: description}} />
+        {this.modal('education')}
+      </div>
+    )
+  }
+
+}
+
+export default EducationAndExperience;
